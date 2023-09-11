9bf06e9c4e522183f261eb1828d93e161a6f153a
Akash singh <akashsisodiya666@gmail.com>
2023-09-11 20:27:45 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link  -->
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- header section starts  -->
+
+<section class="header">
+
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href=" about.php"> about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+
+<!-- header section ends  -->
+
+<div class="heading" style="background:url(images/header-bg-1.jpg) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide service</span>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- about section ends  -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,distinctio? Excepturi 
+                consequuntur tenetur itaque recusandae quis maxime quos nulla. Quam facere nihil maiores
+                consequatur eligendi et repudiandae odio aliquid error.</p>
+                <h3>luna</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, est voluptates? Sit, 
+                reprehenderit voluptatem eaque iusto laboriosam architecto, repellendus laborum accusamus 
+                laudantium soluta amet tempora doloribus, necessitatibus nihil ipsam dolorum tempore optio.
+                Fuga commodi animi ea hic nulla, vitae accusantium!</p>
+                <h3>andrew hawlk</h3>
+                <span>traveler</span>
+                <img src="images/pic-2.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, est voluptates? Sit, 
+                reprehenderit voluptatem eaque iusto laboriosam architecto, repellendus laborum accusamus 
+                laudantium soluta amet tempora doloribus, necessitatibus nihil ipsam dolorum tempore optio.
+                Fuga commodi animi ea hic nulla, vitae accusantium!</p>
+                <h3>andrew hawlk</h3>
+                <span>traveler</span>
+                <img src="images/pic-3.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto aliquid, cumque dolore 
+                quia neque quibusdam. Necessitatibus, dolorem! Consequuntur, quibusdam at!</p>
+                <h3>andrew hawlk</h3>
+                <span>traveler</span>
+                <img src="images/pic-4.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit laborum adipisci facilis et nesciunt autem porro at eius facere nobis. Accusantium eius quod tempora laudantium adipisci minus. Rem, natus? Aliquid explicabo ad reiciendis dolores? Aspernatur, tempore debitis. Totam, nisi tenetur.</p>
+                <h3>andrew hawlk</h3>
+                <span>traveler</span>
+                <img src="images/pic-5.jpg" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id est dolore, commodi vero quis beatae, fugit ullam odio perspiciatis qui cupiditate a ipsa voluptate hic?</p>
+                <h3>andrew hawlk</h3>
+                <span>traveler</span>
+                <img src="images/pic-6.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- reviews section ends  -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">   <i class="fas fa-angle-right"></i> home</a>
+            <a href=" about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php">   <i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +919876543210</a>
+            <a href="#"><i class="fas fa-phone"></i> +919876543211</a>
+            <a href="#"><i class="fas fa-envelope"></i> demo@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> mathura, india - 281406</a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i> linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Akash singh</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
